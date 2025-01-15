23df51b6822e1d7414ccef1e1973726cc4fd65aa
giesf <64958447+giesf@users.noreply.github.com>
2025-01-15 13:32:44 +0100

    added meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -225,6 +225,8 @@ app.get('/', (c) => {
   return c.html(<html>
     <head>
       <title>taskman v0.0.1</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charset="utf-8" />
       <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@materialstyle/materialstyle@3.1.1/dist/css/materialstyle.min.css"
         integrity="sha384-TveZ4SBMG9Zwu44Pq5aK2bgL+4CaFRTtx6pSSsxmQKWhIRKoONDSRW+k+NA9A0Gk"
